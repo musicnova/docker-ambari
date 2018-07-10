0c65e860e9a4996c12552dfb3490357dbd199d4c
YO <musicnova@yandex.ru>
2018-07-10 15:41:03 +0300

    0: up

--- hello/step_1_2.sh
+++ hello/step_1_2.sh
@@ -1,4 +1,53 @@
 #!/bin/sh
 echo "export MYIP=127.0.0.1"
 # Step 2. Register Blueprint with Ambari
-curl -H "X-Requested-By: ambari" -X POST -u admin:admin http://$MYIP:8080/api/v1/blueprints/:myBlueprint
+echo '
+{
+  "host_groups" : [
+    {
+      "name" : "host_group_1",     
+      "components" : [
+        {
+          "name" : "NAMENODE"
+        },
+        {
+          "name" : "SECONDARY_NAMENODE"
+        },       
+        {
+          "name" : "DATANODE"
+        },
+        {
+          "name" : "HDFS_CLIENT"
+        },
+        {
+          "name" : "RESOURCEMANAGER"
+        },
+        {
+          "name" : "NODEMANAGER"
+        },
+        {
+          "name" : "YARN_CLIENT"
+        },
+        {
+          "name" : "HISTORYSERVER"
+        },
+        {
+          "name" : "MAPREDUCE2_CLIENT"
+        },
+        {
+          "name" : "ZOOKEEPER_SERVER"
+        },
+        {
+          "name" : "ZOOKEEPER_CLIENT"
+        }
+      ],
+      "cardinality" : "1"
+    }
+  ],
+  "Blueprints" : {
+    "blueprint_name" : "single-node-hdfs-yarn",
+    "stack_name" : "HDP",
+    "stack_version" : "2.4"
+  }
+}
+' | curl -H "X-Requested-By: ambari" -X POST -d @- -u admin:admin http://$MYIP:8080/api/v1/blueprints/myBlueprint
