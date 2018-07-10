e5a89d052506172db75be37e6bb92250ead95ca5
YO <musicnova@yandex.ru>
2018-07-10 15:22:30 +0300

    0: up

--- hello/step_1_0.sh
+++ hello/step_1_0.sh
@@ -2,4 +2,4 @@
 echo "export MYIP=127.0.0.1"
 
 # Confirm the Agent hosts are registered with the Server.
-curl -H "X-Requested-By: ambari" -X GET -u admin:admin http:$MYIP:8080/api/v1/hosts
+curl -H "X-Requested-By: ambari" -X GET -u admin:admin http://$MYIP:8080/api/v1/hosts
