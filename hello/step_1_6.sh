57650d90e0c9f62f40f46984d65ac0349a32c095
YO <musicnova@yandex.ru>
2018-07-10 16:41:56 +0300

    0: up

--- hello/step_1_6.sh
+++ hello/step_1_6.sh
@@ -5,6 +5,10 @@ echo "export MYIP=127.0.0.1"
 # Step 6. Track installaion
 
 
-curl -H "X-Requested-By: ambari" -X GET -u admin:admin http://$MYIP:8080/api/v1/clusters/mycluster/requests/
+curl -H "X-Requested-By: ambari" -X GET -u admin:admin http://$MYIP:8080/api/v1/clusters/myCluster/requests/
 
-curl -H "X-Requested-By: ambari" -X GET -u admin:admin http://$MYIP:8080/api/v1/clusters/mycluster/requests/1
+echo
+echo "====="
+echo
+
+curl -H "X-Requested-By: ambari" -X GET -u admin:admin http://$MYIP:8080/api/v1/clusters/myCluster/requests/1
