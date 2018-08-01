7ad1e7033bdb217792fb5c677e6dbb98aeeb2580
YO <musicnova@yandex.ru>
2018-08-01 16:55:33 +0300

    0: up

---
+++ kerberos/4_streaming/step_1_2_make_print.sh
@@ -0,0 +1,61 @@
+#!/bin/sh
+echo "export MYIP=127.0.0.1"
+# xhost +local:
+# Step 2. Register Blueprint with Ambari
+cat 4_all/streaming.json | sed -e 's#"Blueprints" : {#"Blueprints" : {"blueprint_name" : "mydefaults_4_streaming",#g' | curl -H "X-Requested-By: ambari" -X POST -d @- -u admin:admin http://$MYIP:8080/api/v1/blueprints/4_all_streaming
+echo 'continue?'
+if [ -n "$DBG" ]; then read ANSWER; else exit 0; fi
+echo '
+{
+  "configurations" : [ ],
+  "host_groups" : [
+    {
+      "name" : "host_group_1",
+      "components" : [
+        {
+          "name" : "NAMENODE"
+        },
+        {
+          "name" : "SECONDARY_NAMENODE"
+        },
+        {
+          "name" : "DATANODE"
+        },
+        {
+          "name" : "HDFS_CLIENT"
+        },
+        {
+          "name" : "RESOURCEMANAGER"
+        },
+        {
+          "name" : "NODEMANAGER"
+        },
+        {
+          "name" : "YARN_CLIENT"
+        },
+        {
+          "name" : "HISTORYSERVER"
+        },
+        {
+          "name" : "APP_TIMELINE_SERVER"
+        },
+        {
+          "name" : "MAPREDUCE2_CLIENT"
+        },
+        {
+          "name" : "ZOOKEEPER_SERVER"
+        },
+        {
+          "name" : "ZOOKEEPER_CLIENT"
+        }
+      ],
+      "cardinality" : "1"
+    }
+  ],
+  "Blueprints" : {
+    "blueprint_name" : "mydefaults_1_sandbox",
+    "stack_name" : "HDP",
+    "stack_version" : "2.3"
+  }
+}'
+cat 4_all/streaming.json | sed -e 's#"Blueprints" : {#"Blueprints" : {"blueprint_name" : "mydefaults_4_streaming",#g' 
