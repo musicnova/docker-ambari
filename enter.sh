22ab63b1672a60b794b04ee3bb91df536b354d7d
YO <musicnova@yandex.ru>
2018-08-17 12:07:07 +0300

    0: up

--- enter.sh
+++ enter.sh
@@ -6,7 +6,7 @@ echo 'A=$(docker ps -a | grep -v "_data" | awk '"'"'NR>1 {print $1}'"')" &&
 echo &&
 echo echo '$A'
 echo &&
-echo B='`'echo '$'A '|' awk -F '"''{'print '$'$N';''}''"''`' &&
+echo B='`'echo '$'A '|' awk "'"'{'print '$'$N';''}'"'"'`' &&
 echo &&
 echo docker exec -it '$B' bash
 echo)
